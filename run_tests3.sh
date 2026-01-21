ede35156cc261196c913b6480abaaea6a65d0404
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-01-21 10:28:14 +0300

    ci: start using a docker file

---
+++ run_tests3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# prepare python environment
+VENV_PATH="/tmp/testgres_venv"
+rm -rf $VENV_PATH
+python -m venv "${VENV_PATH}"
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+source "${VENV_PATH}/bin/activate"
+pip install -r tests/requirements.txt
+
+# check code style
+flake8 .
+
+# run builtin tests
+python3 -m pytest -l -vvv tests
+
+set +eux
