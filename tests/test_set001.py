9c3c0bd8d1c8c87c5d57ba44e071ef8c4f4645d5
d.kovalenko <dmitry.lipetsk@gmail.com>
2026-03-16 08:27:27 +0300

    build: implement dynamic versioning and unified metadata
    - Move version to src/__init__.py as the single source of truth
    - Update pyproject.toml to use dynamic versioning via setuptools

--- tests/test_set001.py
+++ tests/test_set001.py
@@ -1,7 +1,22 @@
 import src.utils as src_utils
+from src import __version__ as package_version
+from packaging.version import Version
 
 
 class TestSet001:
+    def test_packake_version(self):
+        assert type(package_version) is str
+
+        v = Version(package_version)
+
+        # Author: Mark G.
+        assert v.major == 0
+        assert v.minor == 0
+        assert v.micro == 7
+
+        assert str(v) == package_version
+        return
+
     def test_func1(self):
         assert src_utils.func1() == "Hello World!"
         return
